815317cc607ab3f485d02013ab6838a9aecbaaa7
Meaghan Jones and Olivia Hinton <meaghan@>
2016-08-08 16:36:53 -0700

    set up project files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,52 @@
 import { Component } from 'angular2/core';
 
-@Component ({
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+    {{ currentTask.description }}
+  </h3>
+  `
+})
+
+export class TaskListComponent {
+  public taskList: Task [];
+  taskClicked(clickedTask: Task):void {
+    console.log(clickedTask);
+  }
+}
+
+@Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>To-Do List</h1>
+      <task-list [taskList]="tasks"></task-list>
     </div>
   `
 })
 
-export class AppComponent {
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
+}
+
+export class AppComponent {
+  public tasks: Task [];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
 }
